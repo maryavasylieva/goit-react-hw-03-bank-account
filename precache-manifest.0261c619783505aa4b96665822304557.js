8ee31d3b6d24b28daaae2bb674b42b075154292a
maryavasylieva <miheta.laurelin@gmail.com>
2019-08-01 23:58:02 +0300

    Updates

---
+++ precache-manifest.0261c619783505aa4b96665822304557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4212cd166942d002908884eff3768cf4",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "02a12f955a345129aaf7",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "fa4f9968a7795bf1fc5e",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.798ef940.chunk.css"
+  },
+  {
+    "revision": "02a12f955a345129aaf7",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.b8cefa43.chunk.js"
+  },
+  {
+    "revision": "fa4f9968a7795bf1fc5e",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.fbbaca5c.chunk.js"
+  },
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  }
+]);
